5ede13d2f9cf0b935d7e76b3914370d3e8de9cef
LennyAmstutz <lennyamstutz@gmail.com>
2024-11-18 08:26:50 +0100

    LennyAmstutz: Aufgabe 1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,6 +3,20 @@ package main
 import "fmt"
 
 func main() {
+
+	var firstName string = "Lenny"
+	var lastName string = "Lenny"
+
+	var dayOfBirth int = 20
+	var monthOfBirth int = 10
+	var yearOfBirth int = 2007
+
+	var numberOfSiblings int = 1
+
+	var heightInMeters float32 = 1.74
+
+	var zodiacSign string = "Wage"
+
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
