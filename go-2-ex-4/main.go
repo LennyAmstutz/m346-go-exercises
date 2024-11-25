979264d1e091b23452ea26261d058a0998296af4
LennyAmstutz <lennyamstutz@gmail.com>
2024-11-25 09:23:17 +0100

    LennyAmstutz: Kapitel 2 Aufgabe 4

--- go-2-ex-4/main.go
+++ go-2-ex-4/main.go
@@ -1,8 +1,42 @@
 package main
 
+import "fmt"
+
+type Student struct {
+	FirstName string
+	LastName  string
+}
+
+type Class struct {
+	Students []Student
+}
+
 func main() {
-	// TODO: declare a type for Student (with first and last name)
-	// TODO: declare a type for Class (consisting of multiple students)
-	// TODO: declare a map of modules being attended by multiple classes
-	// TODO: output everything using fmt.Println()
+
+	student1 := Student{FirstName: "Lenny", LastName: "Amstutz"}
+	student2 := Student{FirstName: "Dylan", LastName: "Rodrigez"}
+	student3 := Student{FirstName: "Lea", LastName: "Schuerch"}
+	student4 := Student{FirstName: "Remo", LastName: "Albiser"}
+	student5 := Student{FirstName: "Danial", LastName: "Najafi"}
+	student6 := Student{FirstName: "Saskia", LastName: "Tellenbach"}
+
+	class1 := Class{Students: []Student{student1, student2, student3}}
+	class2 := Class{Students: []Student{student4, student5, student6}}
+
+	module := map[string][]Class{
+		"346": {class1},
+		"111": {class1, class2},
+		"222": {class2},
+	}
+
+	for moduleID, classes := range module {
+		fmt.Printf("Modul %s wird von diesen Klassen besucht:\n", moduleID)
+		for i, class := range classes {
+			fmt.Printf("  Klasse %d:\n", i+1)
+			for _, student := range class.Students {
+				fmt.Printf("    - %s %s\n", student.FirstName, student.LastName)
+			}
+		}
+		fmt.Println()
+	}
 }
