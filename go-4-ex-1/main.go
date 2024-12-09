91497e2996b05f7455c794a2ec432ef5062bab18
LennyAmstutz <lennyamstutz@gmail.com>
2024-12-09 09:23:23 +0100

    LennyAmstutz: Kapitel 4 Aufgabe 1, 2, 3, 4

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,43 @@
 package main
 
-// TODO: implement the function computeGrade
+import (
+	"errors"
+	"fmt"
+)
+
+func berechneNote(erreichtePunkte, maxPunkte float64) (float64, error) {
+	if erreichtePunkte < 0 || maxPunkte <= 0 {
+		return 0, errors.New("Punkte müssen positiv sein")
+	}
+	if erreichtePunkte > maxPunkte {
+		return 0, errors.New("erreichte Punkte dürfen nicht größer als max. Punkte sein")
+	}
+
+	note := 1 + 5*(1-(erreichtePunkte/maxPunkte))
+
+	return note, nil
+}
 
 func main() {
-	// TODO: call the function computeGrade
+
+	note1, err1 := berechneNote(17.5, 28.0)
+	if err1 != nil {
+		fmt.Println("Fehler:", err1)
+	} else {
+		fmt.Println("Note:", note1)
+	}
+
+	note2, err2 := berechneNote(25.0, 25.0)
+	if err2 != nil {
+		fmt.Println("Fehler:", err2)
+	} else {
+		fmt.Println("Note:", note2)
+	}
+
+	note3, err3 := berechneNote(-5.0, 20.0)
+	if err3 != nil {
+		fmt.Println("Fehler:", err3)
+	} else {
+		fmt.Println("Note:", note3)
+	}
 }
